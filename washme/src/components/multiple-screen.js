705f156fcb9c9de4a0b2e64be5c158c4d77c1aa0
julien belinga <julienbelinga29@gmail.com>
2023-05-16 00:33:59 +0200

    update

--- washme/src/components/multiple-screen.js
+++ washme/src/components/multiple-screen.js
@@ -15,7 +15,7 @@ export default function MultipleScreen() {
         <TextBlock
           class="text-left"
           title="Devenez prestataire Washme : gagnez de l'argent en faisant une bonne action"
-          description="Vous possédez une machine à laver et vous cherchez un moyen de générer un revenu supplémentaire tout en contribuant à l'écologie ? Washme est l'application qu'il vous faut ! Avec notre plateforme, vous pouvez mettre votre machine à laver à disposition des personnes qui ont du linge sale à laver. En échange de vos services, vous serez rémunéré(e) et pourrez ainsi arrondir vos fins de mois. De plus, vous pourrez rejoindre une communauté active de personnes qui partagent les mêmes valeurs que vous, en termes d'écologie et de partage."
+          description="Vous possédez une machine à laver et vous cherchez un moyen de générer un revenu supplémentaire tout en contribuant à l'écologie ? <br/> Washme est l'application qu'il vous faut ! Avec notre plateforme, vous pouvez mettre votre machine à laver à disposition des personnes qui ont du linge sale à laver. En échange de vos services, vous serez rémunéré(e) et pourrez ainsi arrondir vos fins de mois. De plus, vous pourrez rejoindre une communauté active de personnes qui partagent les mêmes valeurs que vous, en termes d'écologie et de partage."
         />
       </ParallaxLayer>
 
@@ -23,7 +23,7 @@ export default function MultipleScreen() {
         <TextBlock
           class="text-right"
           title="Washme : la solution économique et écologique pour laver votre linge"
-          description="Vous avez du linge sale à laver et vous manquez de temps pour vous en occuper ? Vous cherchez une solution économique et écologique pour faire votre lessive ? Avec Washme, vous pouvez trouver des personnes disposant d'une machine à laver qui pourront prendre en charge votre lessive. En utilisant notre application, vous économiserez du temps et de l'argent tout en contribuant à la préservation de l'environnement. De plus, vous pourrez rejoindre une communauté dynamique de personnes qui partagent les mêmes valeurs que vous."
+          description="Vous avez du linge sale à laver et vous manquez de temps pour vous en occuper ? Vous cherchez une solution économique et écologique pour faire votre lessive ? <br/> Avec Washme, vous pouvez trouver des personnes disposant d'une machine à laver qui pourront prendre en charge votre lessive. En utilisant notre application, vous économiserez du temps et de l'argent tout en contribuant à la préservation de l'environnement. De plus, vous pourrez rejoindre une communauté dynamique de personnes qui partagent les mêmes valeurs que vous."
         />
       </ParallaxLayer>
 
